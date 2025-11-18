8fa59e5779562f2b79c65f99d779f0cb0cd6bb6b
Jeff Zellen <jeff@zellenfamily.com>
2025-11-17 20:49:45 -0500

    Update volleyball_stats.js

--- public/js/volleyball_stats.js
+++ public/js/volleyball_stats.js
@@ -1,8 +1,4 @@
 // public/js/volleyball_stats.js
-var gk_isXlsx = false;
-var gk_xlsxFileLookup = {};
-var gk_fileData = {};
-
 const HOME_TEAM_FALLBACK = 'Home Team';
 const HOME_TEAM_TEMPLATE_PATTERN = /\{homeTeam\}/g;
 let homeTeamName = HOME_TEAM_FALLBACK;
@@ -505,9 +501,8 @@ const apiClient = (() => {
     },
 
     async deleteMatch(id) {
-      const match = getRawMatch(id);
-      if (!match) return null;
-      return await updateMatchInternal(match.id, { ...stripInternalMatch(match), deleted: true });
+      await request(`/api/match/delete/${id}`, { method: 'DELETE' });
+      return { id };
     }
   };
 })();
